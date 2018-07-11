6c70b3e857562801c02b97d572e190550582f5fa
Riley Ashton <rileyashton@me.com>
2018-07-11 14:17:43 -0400

    Update

--- R/Simulation.R
+++ R/Simulation.R
@@ -3,21 +3,22 @@ Simulation <- R6Class("Simulation2tibble",
     SimulationDataGenerator = NULL,
 
 
-    initialize = function(SimulationDataGenerator, stepwise_algos,
+    initialize = function(SimulationDataGenerator, stepwise_constructors,
                           num_simulations, parallel_cores) {
       self$SimulationDataGenerator <- SimulationDataGenerator
-      private$stepwise_algos <- stepwise_algos
+      private$stepwise_constructors <- stepwise_constructors
       private$num_simulations <- num_simulations
       private$parallel_cores <- parallel_cores
     },
 
     simulate = function() {
       private$preallocate_lists()
-      response_name <- self$SimulationDataGenerator$get_response_name()
 
       for(i in seq.int(private$num_simulations)) {
-        sim_data <- self$SimulationDataGenerator$simulate_data()
-        # TODO
+        private$sim_data <- self$SimulationDataGenerator$simulate_data()
+        #private$construct_stepwise_objects(sim_data, )
+
+
       }
     },
 
@@ -34,13 +35,27 @@ Simulation <- R6Class("Simulation2tibble",
     }
   ),
   private = list(
-    stepwise_algos = NULL,
+    stepwise_constructors = NULL,
     num_simulations = NULL,
     parallel_cores = NULL,
     test_holdout_values = NULL,
     inclusion_orders = NULL,
     fitted_coefficients = NULL,
     predictive_power = NULL,
+
+    sim_data = NULL,
+    stepwise_objects = NULL,
+
+    construct_stepwise_objects = function() {
+      response_name <- self$SimulationDataGenerator$get_response_name()
+      starting_formula <- as.formula(paste0(response_name, "~ 1"))
+
+      k <- log(self$SimulationDataGenerator$PredictorsGenerator$get_num_observations())
+      lapply(stepwise_constructors,
+             function(new) new(sim_data, response_name, starting_formula,
+                               stepwise_direction = "forward", k)
+      )
+    },
 
     preallocate_lists = function() {
       private$inclusion_orders = vector(mode = "list", length = private$num_simulations)
